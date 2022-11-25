e091332f87240d083f828d48258f44df06dd0723
AlexKoff88 <alexander.kozlov@intel.com>
2022-11-25 19:31:34 +0400

    Added typehints

--- tv2ov/preprocess_converter.py
+++ tv2ov/preprocess_converter.py
@@ -10,6 +10,22 @@ class PreprocessorConvertor():
 
     @staticmethod
     def from_torchvision(model: ov.Model, input_name: str, transform: Callable, input_example: Any) -> ov.Model:
+        """
+        Convert torchvision transform to OpenVINO preprocessing
+        Arguments:
+            model (ov.Model):
+                Result name
+            input_name (str):
+                Name of the result node to connect with preprocessing
+            transform (Callable):
+                torchvision transform to convert
+            input_example (torch.Tensor or np.ndarray or PIL.Image):
+                Example of input data
+        Returns:
+            ov.Mode: OpenVINO Model object with preprocessing
+        Example:
+            >>> model = PreprocessorConvertor.from_torchvision(model, "input", transform, input_example)
+        """
         try:
             from torchvision import transforms
             from tv2ov.torchvision import from_torchvision
